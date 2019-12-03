2554a44d78c41c6a1af45d9f64a57d4053fa3e1e
Nikesh <Adhikarinikesh49@gmail.com>
2019-12-03 19:36:53 +1100

    file changed to verify for zoho

---
+++ src/pages/zohoverify/verifyforzoho.js
@@ -0,0 +1,6 @@
+import React from "react"
+import Layout from "../../components/layout"
+
+const verifyforzoho = () => <Layout>1575359655372</Layout>
+
+export default verifyforzoho
--- src/pages/zohoverify/zohoverify.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import Layout from "../../components/layout"
-
-const zohoverify = () => <Layout>1575359655372</Layout>
-
-export default zohoverify
